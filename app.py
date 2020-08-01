7f9dba7753b3028054ad236d6efe6c394044a084
Jennifer <jennifer.portinof@utem.cl>
2020-07-31 22:18:19 -0400

    Add: rutas.html

--- app.py
+++ app.py
@@ -15,52 +15,69 @@ def home():
 
 @app.route('/about', methods = ['GET', 'POST'])
 def about():
+    #Variables
     form = UploadForm()
     global filename
+    global num_camiones
+
     if request.method == 'POST' and form.validate_on_submit():
         arch= form.archivo.data
         filename = secure_filename(arch.filename) #Nombre del Archivo 
         arch.save(app.root_path+"./static/archivos/"+filename) #Donde se guarda
+        num_camiones = form.cant_camiones.data
         return redirect(url_for('datos'))
     return render_template("about.html", form=form)
 
 @app.route('/datos', methods = ['GET', 'POST'])
 def datos():
-    form = DetalleForm()
-    Param = lecturaArchivo(filename) #Arreglo de Clases
-    
-    #crea dict tipo {Centro:{PtoVenta:Distancia}}
     #Variables
+    form = DetalleForm()
+    Param = lecturaArchivo("./static/archivos/"+filename) #Arreglo de Clases
     Centros = {} #Dic Centros {N:[x,y]}
     Puntos = {} #Dic Puntos {N:[x,y]}
     aux = Parametros()
     message = ''
+    global conexiones
+    conexiones = {}
 
-    print('Arreglo de clases: ', Param)
-
-    for i in range(0,len(Param)):
-        aux = Param[i]
-        k = aux.N
-        if aux.T == 'C':
-            Centros[k] = [aux.X,aux.Y]
-        else:
-            Puntos[k] = [aux.X,aux.Y]
-    
-    form.centro.choices = [(key, 'C'+str(key)) for key in Centros.keys()]
-    form.punto.choices = [(key, 'P'+str(key)) for key in Puntos.keys()]
-
-    #if request.method == 'POST':
-        #[(i,'q'+str(i-1)) for i in range(len(transitions1)+1)]
-        #trans.origen1.choices[0] = (0,'-')
+    if request.method == 'GET':
+        for i in range(0,len(Param)):
+            aux = Param[i]
+            k = aux.N
+            if aux.T == 'C':
+                Centros[k] = [aux.X,aux.Y]
+            else:
+                Puntos[k] = [aux.X,aux.Y]
+        #print('Arreglo de clases: ', Param)
+        form.camion.choices = [(key+1, str(key+1)) for key in range(num_camiones)]
+        form.centro.choices = [(key, 'C'+str(key)) for key in Centros.keys()]
+        form.punto.choices = [(key, 'P'+str(key)) for key in Puntos.keys()]
 
     if request.method == 'POST' and form.validate_on_submit():
+        ruta = []
         camion = form.camion.data
         centro = form.centro.data
         punto = form.punto.data
         productos = form.productos.data
         message = 'El camion '+str(camion)+' esta asignado a el Centro de distribucion '+str(centro)+' y va al Punto de venta '+str(punto)+' llevando '+str(productos)+' productos'
         
+        for k in range(0,len(Param)):
+            aux = Param[k]
+            if centro==aux.N:
+                ruta.append(aux)
+            if punto==aux.N:
+                ruta.append(aux)
+        
+        ruta.append(productos)
+        conexiones[camion]=ruta
+        return redirect(url_for('rutas'))
+
     return render_template("datos.html", form=form, message=message)
+
+@app.route('/rutas', methods = ['GET', 'POST'])
+def rutas():
+    print(conexiones)
+    return render_template("rutas.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -5,13 +5,14 @@ from wtforms.validators import Required
 
 class UploadForm(FlaskForm):
     archivo = FileField('Archivo', validators = [Required("Tienes que subir el archivo")])
+    cant_camiones = IntegerField('Cantidad Camiones',validators = [])
     
-    submit = SubmitField(' Subir ')
+    submit = SubmitField(' Enviar ')
 
 class DetalleForm(FlaskForm):
-    camion = StringField('Camion', validators = [])
-    centro = SelectField('Centro Distribucion', choices = [(0,'-')])
-    punto = SelectField('Punto de Ventas', choices = [(0,'-')])
+    camion = SelectField('Camion', choices = [(0,'--')])
+    centro = SelectField('Centro Distribucion', choices = [(0,'--')])
+    punto = SelectField('Punto de Ventas', choices = [(0,'--')])
     productos = IntegerField('Cantidad de Productos', validators = [])
 
     submit = SubmitField('+')
