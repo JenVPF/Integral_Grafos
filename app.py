34400a05cc5e7411c3795f284d7a35c7f26db65b
Diego Tapia <diego.tapialeal@utem.cl>
2020-08-03 15:20:39 -0400

    ADD: piloto grafica de rutas con networkx

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_file
+
 #Formulario
 from werkzeug.utils import secure_filename
+
 #Llamada Funciones
 from forms import UploadForm, DetalleForm
 from func import lecturaArchivo, validacionString, validarPuntos, distancia_de_lista, CDconCoordenadasdePV, DistanciasEntreNodos, ordenarCentros, ordenarPuntos
@@ -8,6 +10,12 @@ from config import Config
 import pandas as pd
 app = Flask(__name__)
 app.config.from_object(Config)
+
+#Libreria de grafos
+import matplotlib
+import matplotlib.pyplot as plt
+from io import BytesIO
+import networkx as nx
 
 @app.route('/')
 def home():
@@ -112,6 +120,44 @@ def rutas():
     centros = ordenarCentros(Asignacion)
     puntos = ordenarPuntos(Asignacion)
     return render_template("rutas.html", Asignacion=Asignacion, centros=centros, puntos=puntos)
+
+@app.route('/graph') # se usa una ruta que contiene los datos del grafo para que Flask los compile
+def grafica():
+
+    G = nx.DiGraph() 
+    #iNGRESAR NODO C
+    G.add_node("C1")
+
+    #iNGRESAR NODOS P
+    nodes=G.add_nodes_from(["P1","P2","P3",'P3','P4','P5'])
+
+    #INGRESAR ARISTAS o relaciones con las distancias ya definidas
+    edges = [('P1', 'P2', 1), ('P1', 'P5', 1), ('P2', 'P3', 1), ('P2', 'P4', 1),('C1','P1',1)  ] 
+
+    #se ingresan las aristas a G
+    G.add_weighted_edges_from(edges) 
+
+    #IMPRESION POR CONSOLA
+    print("Nodes of graph: ")
+    print(G.nodes())
+    print("Edges of graph: ")
+    print(G.edges(data=True))
+
+    #plt.figure(figsize =(9, 12)) funcion para caMbiar la diMencion
+
+    nx.draw_networkx(G, with_label = True ,node_color ='green',width=2.0,node_size=600)#Funcion dibujar
+    img = BytesIO()         # se le asigna memoria a la imagen
+    plt.savefig(img) # save as png
+    plt.title('Distancias') #Titulo
+
+    #plt.show() # display
+    img.seek(0) 
+    #plt.clf() # limpia el caché del graficado
+
+    return send_file(img, mimetype = 'image/png') # retorna la imagen que se agrega al html mediante a send_file() función de Flask
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True,port=3000)
